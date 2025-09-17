31c1c5da07db914ecfdb440cb01f973d8d403ccf
ishwar_kalmegh <ishwarkalmegh156@gmail.com>
2025-09-18 00:39:27 +0530

    initialized+nativewind

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "./globals.css"
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-xl font-bold text-red-500">shit mix of emotions.</Text>
+    </View>
+  );
+}
